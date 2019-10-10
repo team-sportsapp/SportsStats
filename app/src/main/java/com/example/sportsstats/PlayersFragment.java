e9f98096fb8018c7785605a5dcf563ba70f2cfb2
atak37 <hkratakii@gmail.com>
2019-10-10 11:51:19 -0700

    player list view and adapter

--- app/src/main/java/com/example/sportsstats/PlayersFragment.java
+++ app/src/main/java/com/example/sportsstats/PlayersFragment.java
@@ -7,12 +7,31 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 
 public class PlayersFragment extends Fragment {
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_players, null);
+        View view = inflater.inflate(R.layout.fragment_players, null);
+
+        //players array
+        String playersArray[] = new String[] {"player_one", "player_two"};
+
+        ListView listView = (ListView) view.findViewById(R.id.playerList);
+
+        //array adapter
+        ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(
+                getActivity(),
+                android.R.layout.simple_list_item_1,
+                playersArray);
+        //adapter setter
+        listView.setAdapter(listViewAdapter);
+
+        return view;
+
+
     }
 }
