0cd5d81b2dbb43e9c9cf3dfd5876d56cd847057c
austonjones <austonjones@yahoo.com>
2020-04-03 17:55:36 -0700

    merging with andy's

--- app/src/main/java/com/example/sportsstats/MainActivity.java
+++ app/src/main/java/com/example/sportsstats/MainActivity.java
@@ -1,5 +1,4 @@
 package com.example.sportsstats;
-
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -17,7 +16,6 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-
 
 public class MainActivity extends AppCompatActivity {
     EditText mpassword,memail;
