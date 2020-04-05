ede1c1511e5b86dcfe93edd048a33c3ab95da9ae
JG <jaysen.gan.586@my.csun.edu>
2020-04-05 13:39:58 -0700

    msg

--- app/src/main/java/com/example/sportsstats/PlayersFragment.java
+++ app/src/main/java/com/example/sportsstats/PlayersFragment.java
@@ -7,6 +7,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,7 +24,7 @@ public class PlayersFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_players, null);
 
-        String[] players = getResources().getStringArray(R.array.players);
+        final String[] players = getResources().getStringArray(R.array.players);
 
         ListView listView = (ListView) view.findViewById(R.id.playerListView);
 
@@ -45,144 +46,51 @@ public class PlayersFragment extends Fragment {
         });
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
-                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                                            @Override
+                                            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                                                //FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                                                //FragmentTransaction transaction = fragmentManager.beginTransaction();
 
-                if (i ==0){
+                                                Fragment playersPopupFragment = new PlayersPopupFragment();
+                                                Bundle args = new Bundle();
+                                                args.putString("Key", players[i]);
+
+                                                playersPopupFragment.setArguments(args);
+                                                getFragmentManager()
+                                                        .beginTransaction()
+                                                        .replace(R.id.fragment_container, playersPopupFragment)
+                                                        .addToBackStack(null)
+                                                        .commit();
+
+                                            }
+                                        });
+        return view;
+
+
+                //transaction.replace(R.id.fragment_container, playersFragment);
+                //transaction.addToBackStack(null);
+                //transaction.commit();
+
+
+
+
+
+                //if (i ==0){
+                    /*
                     Fragment teamsFragment = new TeamsFragment();
                     transaction.replace(R.id.fragment_container, teamsFragment);
                     transaction.addToBackStack(null);
                     transaction.commit();
-                }
-                else if (i == 1){
-                    Fragment settingsFragment = new SettingsFragment();
-                    transaction.replace(R.id.fragment_container, settingsFragment);
+                    */
+
+                    /*
+                    Fragment playersPopupFragment = new PlayersPopupFragment();
+                    transaction.replace(R.id.fragment_container, playersPopupFragment);
                     transaction.addToBackStack(null);
                     transaction.commit();
-                }
-                else if (i == 2){
-                    Intent intent = new Intent(getActivity(), MainActivity.class);
-                    startActivity(intent);
-                }
-                else if (i == 3){
+                    */
+                //Fragment playersPopupFragment = (Fragment) getSupportFragmentManager.
 
-                }
-                else if (i == 4){ //Chicago Bulls
 
-                }
-                else if (i == 5){ //Cleveland Cavaliers
-
-                }
-                else if (i == 6){ //Dallas Mavericks
-
-                }
-                else if (i == 7){ //Denver Nuggets
-
-                }
-                else if (i == 8){ //Detroit Pistons
-
-                }
-                else if (i == 9){ //Golden State Warriors
-
-                }
-                else if (i == 10){ //Houston Rockets
-
-                }
-                else if (i == 11){ //Indiana Pacers
-
-                }
-                else if (i == 12){ //LA Clippers
-
-                }
-                else if (i == 13){ //LA Lakers
-
-                }
-                else if (i == 14){ //Memphis Grizzlies
-
-                }
-                else if (i == 15){ //Miami Heat
-
-                }
-                else if (i == 16){ //Milwaukee Bucks
-
-                }
-                else if (i == 17){ //Minnesota Timberwolves
-
-                }
-                else if (i == 18){ //New Orleans Pelicans
-
-                }
-                else if (i == 19){ //New York Knicks
-
-                }
-                else if (i == 20){ //Oklahoma City Thunder
-
-                }
-                else if (i == 21){ //Orlando Magic
-
-                }
-                else if (i == 22){ //Philadelphia Sixers
-
-                }
-                else if (i == 23){ //Phoenix Suns
-
-                }
-                else if (i == 24){ //Portland Tail Blazers
-
-                }
-                else if (i == 25){ //Sacramento Kings
-
-                }
-                else if (i == 26){ //San Antonio Spurs
-
-                }
-                else if (i == 27){ //Toronto Raptors
-
-                }
-                else if (i == 28){ //Utah Jazz
-
-                }
-                else if (i == 29){ //Washington Wizards
-
-                }
-                else if (i == 30){ //Washington Wizards
-
-                }
-                else if (i == 31){ //Washington Wizards
-
-                }
-                else if (i == 32){ //Washington Wizards
-
-                }
-                else if (i == 33){ //Washington Wizards
-
-                }
-                else if (i == 34){ //Washington Wizards
-
-                }
-                else if (i == 35){ //Washington Wizards
-
-                }
-                else if (i == 36){ //Washington Wizards
-
-                }
-                else if (i == 37){ //Washington Wizards
-
-                }
-                else if (i == 38){ //Washington Wizards
-
-                }
-                else if (i == 39){ //Washington Wizards
-
-                }
-                else if (i == 40){ //Washington Wizards
-
-                }
-            }
-        });
-
-        return view;
     }
 }
