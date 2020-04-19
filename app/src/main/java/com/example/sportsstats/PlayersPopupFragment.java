d08b465123e04d51bf3153eafddd499f50db24d0
JG <jaysen.gan.586@my.csun.edu>
2020-04-19 16:13:56 -0700

    pushing as backup

---
+++ app/src/main/java/com/example/sportsstats/PlayersPopupFragment.java
@@ -0,0 +1,190 @@
+package com.example.sportsstats;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlayersPopupFragment extends Fragment {
+
+    public PlayersPopupFragment(){}
+
+    TextView tvPlayerNamePop, tvPositionPop, tvFieldGoalPercentPop, tvPointsPop, tvThreePointMakesPop, tvFreeThrowPercentPop, tvReboundsPop, tvAssistPop, tvStealsPop, tvBlocksPop, tvTurnoversPop;
+    String playerName= "";
+    String playerPosition= "";
+
+    private static DecimalFormat df = new DecimalFormat("0.00");
+
+    double playerPoints = 0;
+    double playerfieldGoalPercent = 0.00;
+    double playerThrees = 0;
+    double playerFreeThrowPercent = 0;
+    double playerRebounds = 0;
+    double playerAssist = 0;
+    double playerSteals = 0;
+    double playerBlocks = 0;
+    double playerTurnovers = 0;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.players_popup, container, false);
+
+        //TextView textView = (TextView)view.findViewById(R.id.tvPlayerNamePop);
+        view.setBackgroundColor(Color.WHITE);
+        String key = getArguments().getString("Key"); //ex: key = "Adams, Steven"
+        //textView.setText(key);
+
+        String[] nameArray = key.split(", "); // [Adams, Steven]
+        playerName = nameArray[1] + " " + nameArray[0]; // "Steven Adams"
+
+        //readPlayerDataPop(playerName);
+
+        TextView pName = (TextView)view.findViewById(R.id.tvPlayerNamePop);
+        //tvPlayerNamePop = new TextView(getActivity());
+        pName.setText(playerName);
+        pName.setTextSize(30);
+        pName.setTextColor(Color.rgb(0,0,0));
+/*
+        tvPositionPop.setText("Position: " +playerPosition);
+        tvPositionPop.setTextSize(20);
+        tvPositionPop.setTextColor(Color.rgb(0,0,0));
+
+        tvFieldGoalPercentPop.setText("FG %: " + String.valueOf(playerfieldGoalPercent));
+        tvFieldGoalPercentPop.setTextSize(20);
+        tvFieldGoalPercentPop.setTextColor(Color.rgb(0,0,0));
+
+        tvThreePointMakesPop.setText("3-Pointers: " +String.valueOf(playerThrees));
+        tvThreePointMakesPop.setTextSize(20);
+        tvThreePointMakesPop.setTextColor(Color.rgb(0,0,0));
+
+        tvPointsPop.setText("Points: " +String.valueOf(playerPoints));
+        tvPointsPop.setTextSize(20);
+        tvPointsPop.setTextColor(Color.rgb(0,0,0));
+
+        tvFreeThrowPercentPop.setText("FT %: " +String.valueOf(playerFreeThrowPercent));
+        tvFreeThrowPercentPop.setTextSize(20);
+        tvFreeThrowPercentPop.setTextColor(Color.rgb(0,0,0));
+
+        tvReboundsPop.setText("Rebounds: "+String.valueOf(playerRebounds));
+        tvReboundsPop.setTextSize(20);
+        tvReboundsPop.setTextColor(Color.rgb(0,0,0));
+
+        tvAssistPop.setText("Assists: "+String.valueOf(playerAssist));
+        tvAssistPop.setTextSize(20);
+        tvAssistPop.setTextColor(Color.rgb(0,0,0));
+
+        tvStealsPop.setText("Steals: "+String.valueOf(playerSteals));
+        tvStealsPop.setTextSize(20);
+        tvStealsPop.setTextColor(Color.rgb(0,0,0));
+
+        tvBlocksPop.setText("Blocks: "+String.valueOf(playerBlocks));
+        tvBlocksPop.setTextSize(20);
+        tvBlocksPop.setTextColor(Color.rgb(0,0,0));
+
+        tvTurnoversPop.setText("Turnovers: "+String.valueOf(playerTurnovers));
+        tvTurnoversPop.setTextSize(20);
+        tvTurnoversPop.setTextColor(Color.rgb(0,0,0));
+*/
+        return view;
+    }
+/*
+    private List<playerSample> playerSamples = new ArrayList<>();
+
+    private void readPlayerDataPop(String correctPlayer) {
+        InputStream is = getResources().openRawResource(R.raw.neweststats);
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(is, Charset.forName("UTF-8"))
+        );
+
+        String line = "";
+
+        try{
+            //step over header
+            reader.readLine();
+            while ((line = reader.readLine()) != null){
+                //Log.d("MyActivity", "Line: " + line);
+                //split by commas
+                String[] tokens = line.split(",");
+
+                //read data
+                playerSample player = new playerSample();
+                reader.readLine();
+
+                player.setPlayer(tokens[0]);
+                if(correctPlayer.equals (tokens[0]))
+                { playerName = correctPlayer;
+                    playerPosition = tokens[1];
+                    playerfieldGoalPercent =Double.parseDouble(tokens[2]);
+                    playerFreeThrowPercent = Double.parseDouble(tokens[3]);
+                    playerThrees = Double.parseDouble(tokens[4]);
+                    playerPoints = Double.parseDouble(tokens[5]);
+                    playerRebounds = Double.parseDouble(tokens[6]);
+                    playerAssist = Double.parseDouble(tokens[7]);
+                    playerSteals = Double.parseDouble(tokens[8]);
+                    playerBlocks = Double.parseDouble(tokens[9]);
+                    playerTurnovers = Double.parseDouble(tokens[10]);
+                }
+
+
+                if(tokens[1].length() > 0){
+                    player.setPosition(tokens[1]); }
+                else {player.setPosition("");}
+                if(tokens[2].length()>0){
+                    player.setFieldGoalPercent(Double.parseDouble(tokens[2]));}
+                else {player.setFieldGoalPercent(0);}
+
+                if(tokens[3].length()>0){
+                    player.setFreeThrowpercent(Double.parseDouble(tokens[3])); }
+                else {player.setFreeThrowpercent(0);}
+
+                if(tokens[4].length()>0){
+                    player.setThreePointersPerGame(Double.parseDouble(tokens[4]));}
+                else {player.setThreePointersPerGame(0);}
+
+                if(tokens[5].length()>0){
+                    player.setPointsPerGame(Double.parseDouble(tokens[5]));}
+                else {player.setPointsPerGame(0);}
+
+                if(tokens[6].length()>0){
+                    player.setReboundsPerGame(Double.parseDouble(tokens[6]));}
+                else {player.setReboundsPerGame(0);}
+
+                if(tokens[7].length()>0){
+                    player.setAssistPerGame(Double.parseDouble(tokens[7]));}
+                else {player.setAssistPerGame(0);}
+
+                if(tokens[8].length()>0){
+                    player.setStealsPerGame(Double.parseDouble(tokens[8]));}
+                else {player.setStealsPerGame(0);}
+
+                if(tokens[9].length()>0){
+                    player.setBlocksPerGame(Double.parseDouble(tokens[9]));}
+                else {player.setBlocksPerGame(0);}
+
+                if(tokens.length>= 11 && tokens[10].length()>0){
+                    player.setTurnoversPerGame(Double.parseDouble(tokens[10]));}
+                else {player.setTurnoversPerGame(0);}
+
+            }
+        }catch (IOException e){
+            Log.wtf("MyActivity", "Error reading data file on line " + line, e);
+            e.printStackTrace();
+        }
+    }
+*/
+}
---
+++ app/src/main/java/com/example/sportsstats/teamSample.java
@@ -0,0 +1,108 @@
+package com.example.sportsstats;
+
+class teamSample {
+    private String team;
+    private double gamesPlayed;
+    private double fieldGoalPercent;
+    private double freeThrowpercent;
+    private double threePointersPerGame;
+    private double pointsPerGame;
+    private double reboundsPerGame;
+    private double assistPerGame;
+    private double stealsPerGame;
+    private double blocksPerGame;
+    private double turnoversPerGame;
+
+    public String getTeam(){
+        return team;
+    }
+    public void setTeam(String player){
+        this.team = player;
+    }
+
+    public double getGamesPlayed(){
+        return gamesPlayed;
+    }
+    public void setGamesPlayed(double gamesPlayed){
+        this.gamesPlayed = gamesPlayed;
+    }
+    public double getFieldGoalPercent(){
+        return fieldGoalPercent;
+    }
+    public void setFieldGoalPercent(double fieldGoalPercent){
+        this.fieldGoalPercent = fieldGoalPercent;
+    }
+    public double getFreeThrowpercent(){
+        return freeThrowpercent;
+    }
+    public void setFreeThrowpercent(double freeThrowpercent){
+        this.freeThrowpercent = freeThrowpercent;
+    }
+
+
+    public double getThreePointersPerGame(){
+        return threePointersPerGame;
+    }
+    public void setThreePointersPerGame(double threePointersPerGame){
+        this.threePointersPerGame = threePointersPerGame;
+    }
+
+    public double getPointsPerGame(){
+        return pointsPerGame;
+    }
+    public void setPointsPerGame(double pointsPerGame){
+        this.pointsPerGame = pointsPerGame;
+    }
+
+    public double getReboundsPerGame(){
+        return reboundsPerGame;
+    }
+    public void setReboundsPerGame(double reboundsPerGame) {
+        this.reboundsPerGame = reboundsPerGame;
+    }
+
+    public double getAssistPerGame(){
+        return assistPerGame;
+    }
+    public void setAssistPerGame(double assistPerGame){
+        this.assistPerGame = assistPerGame;
+    }
+
+    public double getStealsPerGame(){
+        return stealsPerGame;
+    }
+    public void setStealsPerGame(double stealsPerGame){
+        this.stealsPerGame = stealsPerGame;
+    }
+
+    public double getBlocksPerGame(){
+        return blocksPerGame;
+    }
+    public void setBlocksPerGame(double blocksPerGame){
+        this.blocksPerGame = blocksPerGame;
+    }
+
+    public double getTurnoversPerGame(){
+        return turnoversPerGame;
+    }
+    public void setTurnoversPerGame(double turnoversPerGame){
+        this.turnoversPerGame = turnoversPerGame;
+    }
+
+    @Override
+    public String toString() {
+        return "playerSample{" +
+                "team='" + team + '\'' +
+                ", gamesPlayed='" + gamesPlayed + '\'' +
+                ", fieldGoalPercent=" + fieldGoalPercent +
+                ", freeThrowpercent=" + freeThrowpercent +
+                ", threePointersPerGame=" + threePointersPerGame +
+                ", pointsPerGame=" + pointsPerGame +
+                ", reboundsPerGame=" + reboundsPerGame +
+                ", assistPerGame=" + assistPerGame +
+                ", stealsPerGame=" + stealsPerGame +
+                ", blocksPerGame=" + blocksPerGame +
+                ", turnoversPerGame=" + turnoversPerGame +
+                '}';
+    }
+}
