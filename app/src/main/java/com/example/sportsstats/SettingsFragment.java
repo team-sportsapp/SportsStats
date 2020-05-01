9cc988a62e203f98baeedb423f663c816c658da0
Andy <hkratakii@gmail.com>
2020-05-01 14:55:20 -0700

    set up logo, making settings fragment

--- app/src/main/java/com/example/sportsstats/SettingsFragment.java
+++ app/src/main/java/com/example/sportsstats/SettingsFragment.java
@@ -7,12 +7,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class SettingsFragment extends Fragment {
-
+    Button button;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_settings, null);
+        button = (Button) findViewById(R.id.logoutBtn);
     }
 }
